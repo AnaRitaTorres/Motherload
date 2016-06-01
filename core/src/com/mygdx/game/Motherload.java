78db9508e4e5ae3e6a17313294f4662fd44ddb28
AnaRitaTorres <anaritatorres96@outlook.pt>
2016-06-01 23:36:11 +0100

    changes screen , shitty menu image still though

--- core/src/com/mygdx/game/Motherload.java
+++ core/src/com/mygdx/game/Motherload.java
@@ -6,7 +6,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.gui.MenuScreen;
 import com.mygdx.game.gui.PlayScreen;
+import com.mygdx.game.logic.MenuState;
 import com.mygdx.game.logic.PlayState;
 
 public class Motherload extends Game
@@ -16,13 +18,14 @@ public class Motherload extends Game
 	public static final float PPM = 16;
 	public SpriteBatch batch;
 	public PlayState pState;
+	public MenuState mState;
 
 	@Override
 	public void create ()
 	{
 		batch = new SpriteBatch();
 		pState = new PlayState();
-		setScreen(new PlayScreen(pState, this));
+		setScreen(new MenuScreen(mState, this));
 	}
 
 	@Override
--- core/src/com/mygdx/game/gui/MenuScreen.java
+++ core/src/com/mygdx/game/gui/MenuScreen.java
@@ -1,11 +1,15 @@
 package com.mygdx.game.gui;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.Motherload;
 import com.mygdx.game.logic.MenuState;
+import com.mygdx.game.logic.PlayState;
 
 /**
  * Created by Rita on 01/06/2016.
@@ -18,13 +22,25 @@ public class MenuScreen implements Screen
     private OrthographicCamera menuCam;
     private Viewport menuPort;
 
+    PlayState play_state;
+
+    Texture background;
+
     public MenuScreen(MenuState menu_state, Motherload game)
     {
         this.menu_state = menu_state;
         this.game = game;
+        play_state = new PlayState();
+
+        background = new Texture("badlogic.jpg");
 
         menuCam = new OrthographicCamera();
+        menuCam.setToOrtho(false, Motherload.V_WIDTH/ Motherload.PPM,  Motherload.V_HEIGHT/ Motherload.PPM);
         menuPort = new FitViewport(Motherload.V_WIDTH/ Motherload.PPM, Motherload.V_HEIGHT/ Motherload.PPM, menuCam);
+
+       // menuCam = new OrthographicCamera();
+
+       // menuPort = new FitViewport(1920,1030, menuCam);
     }
 
     @Override
@@ -34,7 +50,21 @@ public class MenuScreen implements Screen
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        menuCam.update();
+        game.batch.setProjectionMatrix(menuCam.combined);
+
+        game.batch.begin();
+        game.batch.draw(background, 0, 0);
+        game.batch.end();
+
+        if (Gdx.input.isTouched())
+        {
+            game.setScreen(new PlayScreen(play_state, game));
+            dispose();
+        }
     }
 
     @Override
@@ -49,7 +79,8 @@ public class MenuScreen implements Screen
     }
 
     @Override
-    public void resume() {
+    public void resume()
+        {
 
     }
 
@@ -59,7 +90,8 @@ public class MenuScreen implements Screen
     }
 
     @Override
-    public void dispose() {
-
+    public void dispose()
+    {
+        background.dispose();
     }
 }
