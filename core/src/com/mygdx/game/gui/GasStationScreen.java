716be047bb22423631deda698a97f46d70717065
AnaRitaTorres <anaritatorres96@outlook.pt>
2016-06-04 17:35:45 +0100

    menu principal done

--- core/src/com/mygdx/game/gui/GasStationScreen.java
+++ core/src/com/mygdx/game/gui/GasStationScreen.java
@@ -1,23 +1,134 @@
-package com.mygdx.game.gui;
-
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.game.Motherload;
+import com.mygdx.game.gui.PlayScreen;
+import com.mygdx.game.logic.GasStation;
+import com.mygdx.game.logic.MenuState;
+import com.mygdx.game.logic.PlayState;
 
-/**
- * Created by Rita on 03/06/2016.
- */
-public class GasStationScreen implements Screen {
+public class GasStationScreen implements Screen
+{
+
+    private static final int BUTTON_WIDTH = 250;
+    private static final int BUTTON_HEIGHT = 50;
+
+
+    MenuState menu_state;
+    Motherload game;
+
+    private OrthographicCamera menuCam;
+    private Viewport menuPort;
+
+    PlayState play_state;
+
+    Stage stage;
+    Skin skin;
+
+    Texture background;
+    Texture driller;
+
+    TextButton titleButton;
+    TextButton playButton;
+    TextButton optionsButton;
+    TextButton exitButton;
+
+    public void GasStationScreen()
+    {
+        this.menu_state = menu_state;
+        this.game = game;
+        play_state = new PlayState();
+        create();
+        handleInput();
+
+        background = new Texture("map.png");
+        driller = new Texture("motherload_sprites/ground_right.png");
+    }
+
+    public void create()
+    {
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        skin = new Skin();
+
+        Pixmap pixmap = new Pixmap(BUTTON_WIDTH, BUTTON_HEIGHT, Pixmap.Format.RGB888);
+        pixmap.setColor(Color.GREEN);
+        pixmap.fill();
+
+        skin.add("white", new Texture(pixmap));
+
+        BitmapFont bfont=new BitmapFont();
+        skin.add("default",bfont);
+
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
+        textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+
+        textButtonStyle.font = skin.getFont("default");
+
+        skin.add("default", textButtonStyle);
+
+        //Define title button
+        titleButton = new TextButton("MOTHERLOAD",textButtonStyle);
+        titleButton.setPosition(180,400);
+        stage.addActor(titleButton);
+
+        //Define play button
+        playButton = new TextButton("PLAY",textButtonStyle);
+        playButton.setPosition(40, 300);
+        stage.addActor(playButton);
+
+        //Define options Button
+        optionsButton = new TextButton("OPTIONS",textButtonStyle);
+        optionsButton.setPosition(40, 200);
+        stage.addActor(optionsButton);
+
+        //Define Exit button
+        exitButton = new TextButton("EXIT",textButtonStyle);
+        exitButton.setPosition(40, 100);
+        stage.addActor(exitButton);
+
+    }
+
+
     @Override
     public void show() {
 
     }
 
     @Override
-    public void render(float delta) {
+    public void render(float delta_time)
+    {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.begin();
+        game.batch.draw(background, 0, 0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        game.batch.draw(driller,250,65,driller.getWidth()*4,driller.getHeight()*4);
+        game.batch.end();
+
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
 
     }
 
     @Override
-    public void resize(int width, int height) {
+    public void resize(int width, int height)
+    {
 
     }
 
@@ -37,7 +148,34 @@ public class GasStationScreen implements Screen {
     }
 
     @Override
-    public void dispose() {
+    public void dispose()
+    {
+        stage.dispose();
+        skin.dispose();
+    }
 
+
+    public void handleInput()
+    {
+
+        playButton.addListener(new ChangeListener()
+        {
+            public void changed (ChangeEvent event, Actor actor)
+            {
+                game.setScreen(new PlayScreen(play_state, game));
+            }
+        });
+
+        optionsButton.addListener(new ChangeListener() {
+            public void changed (ChangeEvent event, Actor actor) {
+                //TODO options screen
+            }
+        });
+
+        exitButton.addListener(new ChangeListener() {
+            public void changed (ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
     }
 }
--- core/src/com/mygdx/game/gui/MenuScreen.java
+++ core/src/com/mygdx/game/gui/MenuScreen.java
@@ -2,18 +2,23 @@ package com.mygdx.game.gui;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.viewport.ExtendViewport;
-import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.Motherload;
-import com.mygdx.game.logic.Buttons;
 import com.mygdx.game.logic.MenuState;
 import com.mygdx.game.logic.PlayState;
 
@@ -21,8 +26,13 @@ import com.mygdx.game.logic.PlayState;
 /**
  * Created by Rita on 01/06/2016.
  */
-public class MenuScreen implements Screen,InputHandler
+public class MenuScreen implements Screen
 {
+
+    private static final int BUTTON_WIDTH = 250;
+    private static final int BUTTON_HEIGHT = 50;
+
+
     MenuState menu_state;
     Motherload game;
 
@@ -31,67 +41,77 @@ public class MenuScreen implements Screen,InputHandler
 
     PlayState play_state;
 
+    Stage stage;
+    Skin skin;
+
     Texture background;
     Texture driller;
-    Texture title;
 
-    Buttons playButton,exitButton;
+    TextButton titleButton;
+    TextButton playButton;
+    TextButton optionsButton;
+    TextButton exitButton;
 
-    Rectangle r = new Rectangle();
-    Rectangle r1 = new Rectangle();
-
-    public MenuScreen(MenuState menu_state, Motherload game)
+     public MenuScreen(MenuState menu_state, Motherload game)
     {
         this.menu_state = menu_state;
         this.game = game;
         play_state = new PlayState();
-        playButton = new Buttons("play.png",40,230);
-        exitButton = new Buttons ("exit.png",40,130);
+        create();
+        handleInput();
 
-        r.set(playButton.getCoordinates().x,playButton.getCoordinates().y,playButton.getWidth(),playButton.getHeight());
-        r1.set(exitButton.getCoordinates().x,exitButton.getCoordinates().y,exitButton.getWidth(),exitButton.getHeight());
-
-        background = new Texture("map2.png");
+        background = new Texture("map.png");
         driller = new Texture("motherload_sprites/ground_right.png");
-        title = new Texture("titulo.png");
-
-        menuCam = new OrthographicCamera();
-        menuCam.setToOrtho(false,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-        menuPort = new FillViewport(Gdx.graphics.getWidth(),Gdx.graphics.getHeight(), menuCam);
-        menuPort.apply(true);
-
     }
 
-    public void update(float delta_time)
+    public void create()
     {
-        Vector2 v = inputHandler();
-        Vector3 b = new Vector3(v.x,v.y,0);
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
 
+        skin = new Skin();
 
-        System.out.print(r.getX() + " " + r.getY() + " " + r.getWidth() + "  " + r.getHeight() + "     ");
+        Pixmap pixmap = new Pixmap(BUTTON_WIDTH, BUTTON_HEIGHT, Pixmap.Format.RGB888);
+        pixmap.setColor(Color.GREEN);
+        pixmap.fill();
 
-        if (r.contains(b.x,b.y))
-        {
-            Gdx.app.log("Mouse Clicked", "");
-            game.setScreen(new PlayScreen(play_state, game));
-            dispose();
+        skin.add("white", new Texture(pixmap));
 
-        }
+        BitmapFont bfont=new BitmapFont();
+        skin.add("default",bfont);
+
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
+        textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+
+        textButtonStyle.font = skin.getFont("default");
+
+        skin.add("default", textButtonStyle);
+
+        //Define title button
+        titleButton = new TextButton("MOTHERLOAD",textButtonStyle);
+        titleButton.setPosition(180,400);
+        stage.addActor(titleButton);
+
+        //Define play button
+        playButton = new TextButton("PLAY",textButtonStyle);
+        playButton.setPosition(40, 300);
+        stage.addActor(playButton);
+
+        //Define options Button
+        optionsButton = new TextButton("OPTIONS",textButtonStyle);
+        optionsButton.setPosition(40, 200);
+        stage.addActor(optionsButton);
+
+        //Define Exit button
+        exitButton = new TextButton("EXIT",textButtonStyle);
+        exitButton.setPosition(40, 100);
+        stage.addActor(exitButton);
 
     }
-    public Vector2 inputHandler()
-    {
-        Vector2 v = new Vector2();
 
-        if (Gdx.input.isTouched())
-        {
-            v.set(Gdx.input.getX(),Gdx.input.getY());
-            Vector3 b = new Vector3(v.x,v.y,0);
-            menuCam.unproject(b);
-        }
-        else v = new Vector2(-1000,-1000);
-        return v;
-    }
 
     @Override
     public void show() {
@@ -101,25 +121,23 @@ public class MenuScreen implements Screen,InputHandler
     @Override
     public void render(float delta_time)
     {
-        update(delta_time);
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        menuCam.update();
-        game.batch.setProjectionMatrix(menuCam.combined);
         game.batch.begin();
-        game.batch.draw(background, 0, 0);
-        game.batch.draw(driller,400,0,driller.getWidth()*2,driller.getHeight()*2);
-        game.batch.draw(title,40,370,title.getWidth()/1.5f,title.getHeight()/1.5f);
-        game.batch.draw(playButton.getButtonTex(),playButton.getCoordinates().x,playButton.getCoordinates().y, playButton.getWidth()/1.8f,playButton.getHeight()/1.8f);
-        game.batch.draw(exitButton.getButtonTex(),exitButton.getCoordinates().x,exitButton.getCoordinates().y, exitButton.getWidth()/1.8f,exitButton.getHeight()/1.8f);
+        game.batch.draw(background, 0, 0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        game.batch.draw(driller,250,65,driller.getWidth()*4,driller.getHeight()*4);
         game.batch.end();
+
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
 
     }
 
     @Override
-    public void resize(int width, int height) {
-        menuPort.update(width, height);
+    public void resize(int width, int height)
+    {
+
     }
 
     @Override
@@ -140,6 +158,32 @@ public class MenuScreen implements Screen,InputHandler
     @Override
     public void dispose()
     {
-        background.dispose();
+        stage.dispose();
+        skin.dispose();
+    }
+
+
+    public void handleInput()
+    {
+
+        playButton.addListener(new ChangeListener()
+        {
+            public void changed (ChangeEvent event, Actor actor)
+            {
+                game.setScreen(new PlayScreen(play_state, game));
+            }
+        });
+
+        optionsButton.addListener(new ChangeListener() {
+            public void changed (ChangeEvent event, Actor actor) {
+                //TODO options screen
+            }
+        });
+
+         exitButton.addListener(new ChangeListener() {
+            public void changed (ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
     }
 }
