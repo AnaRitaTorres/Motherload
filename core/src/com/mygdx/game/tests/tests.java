a2ae62bb35919eb0e68b4eabd28440d4d0f2d8b2
AnaRitaTorres <anaritatorres96@outlook.pt>
2016-06-06 16:50:30 +0100

    teste 1==1 , works

---
+++ core/src/com/mygdx/game/tests/tests.java
@@ -0,0 +1,17 @@
+package com.mygdx.game.tests;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ * Created by Rita on 06/06/2016.
+ */
+public class tests
+{
+    @Test
+    public void oneEqualsOne()
+    {
+        assertEquals(1, 1);
+    }
+
+}
