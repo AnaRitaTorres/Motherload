d52a1b35e1bd21b8e19525d9e7cfbc20b5d57573
AnaRitaTorres <anaritatorres96@outlook.pt>
2016-06-06 17:15:32 +0100

    libgdx runner

--- core/src/com/mygdx/game/tests/tests.java
+++ core/src/com/mygdx/game/tests/tests.java
@@ -1,11 +1,15 @@
 package com.mygdx.game.tests;
 
+import com.mygdx.game.GdxTestRunner;
+
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Created by Rita on 06/06/2016.
  */
+@RunWith(GdxTestRunner.class)
 public class tests
 {
     @Test
