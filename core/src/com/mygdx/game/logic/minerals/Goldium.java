3e71e35c0ce67b3e84629f9ebbce070f354ab932
dalugoga <dalugoga@gmail.com>
2016-06-17 12:18:01 +0100

    that golden fail

--- core/src/com/mygdx/game/logic/minerals/Goldium.java
+++ core/src/com/mygdx/game/logic/minerals/Goldium.java
@@ -1,6 +1,7 @@
 package com.mygdx.game.logic.minerals;
 
 import com.badlogic.gdx.maps.MapObject;
+import com.mygdx.game.Motherload;
 import com.mygdx.game.logic.Map;
 import com.mygdx.game.logic.Mineral;
 import com.mygdx.game.logic.PlayState;
@@ -19,10 +20,18 @@ public class Goldium extends Mineral
     public Goldium(PlayState play_state,Map map, MapObject object)
     {
         super(play_state, map, object, 20, 250,1250);
+        fixture.setUserData(this);
+        setCategoryFilter(Motherload.MINERAL_BIT);
     }
 
     @Override
     public void drill() {
+        setCategoryFilter(Motherload.DESTROYED_BIT);
+        getCell().setTile(null);
+
+        play_state.score = play_state.score + points;
+
+        play_state.getDriller().addMineral(this);
 
     }
 }
